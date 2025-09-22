974d8a63185106edab066177f3521b70eed10b6b
Yuma Ishikawa <yuma.fuu05@gmail.com>
2025-09-22 14:30:29 +0900

    fix/auth

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,12 +1,10 @@
 provider "kwgithub" {
   token = var.github_token
+  owner = "knowledge-work"
 }
 
 # GitHub App authentication example
 provider "kwgithub" {
-  app_auth {
-    id              = var.github_app_id
-    installation_id = var.github_app_installation_id
-    pem_file        = var.github_app_pem_file
-  }
+  owner = "knowledge-work"
+  app_auth {}
 }
